7b80d5c0209dc9d947c4808516c17dfa3f23401a
Justin Bachus <justin@slcflightdeals.com>
2017-08-15 22:43:39 -0600

    Add a grace period before starting django

--- helloworld/entrypoint.sh
+++ helloworld/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/dumb-init /bin/ash
+
+#Give DBs 5 seconds to start before running migrations
+sleep 5
+
+#Run DB migrations
 python3 manage.py migrate
+
+#Start the server
 python3 manage.py runserver 0.0.0.0:8000
